ca265f443b729f141d52661910cdb90e0f5220ce
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-12-06 11:22:06 -0800

    AirBnB first touch

---
+++ models/base_model.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" Defines BaseModel class"""
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """ Base model class that defines all common attributes/methods for other classes"""
+
+
+    def __init__(self, *args, **kwargs):
+        """ init method for the base class"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def save(self):
+        """ Save method to update public istance attribute updated_at"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """ Returns a dictionary"""
+        instance_dict = self.__dict__.copy()
+        instance_dict["created_at"] = self.created_at.isoformat()
+        instance_dict["updated_at"] = self.updated_at.isoformat()
+        instance_dict["__class__"] = self.__class__.__name__
+
+        for key, value in self.__dict__.items():
+            if key not in {'created_at', 'updated_at'} and key not in instance_dict:
+                instance_dict[key] = value
+        return instance_dict
+
+
+    def __str__(self):
+        """defines classe"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__))
+
