c9ca6346669e2d15fc94ed7c75347c96d63437ec
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-12-11 03:40:15 -0800

    console

--- console.py
+++ console.py
@@ -92,5 +92,41 @@ class HBNBCommand(cmd.Cmd):
                     objl.append(obj.__str__())
             print(objl)
 
+    def do_update(self, line):
+        """ Usage: update <class name> <id> <attribute name>
+        "<attribute value>"""
+        args = line.split()
+        objdict = storage.all()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif len(args) > 0 and args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(args[0], args[1]) not in objdict:
+            print("** no instance found **")
+        elif len(args) == 2:
+            print("** attribute name missing **")
+        elif len(args) == 3:
+            print("** value missing **")
+        else:
+            if len(args) == 4:
+                obj = objdict["{}.{}".format(args[0], args[1])]
+                if args[2] in obj.__class__.__dict__.keys():
+                    valtype = type(obj.__class__.__dict__[args[2]])
+                    obj.__dict__[args[2]] = valtype(args[3])
+                else:
+                    obj.__dict__[args[2]] = args[3]
+            elif type(eval(args[2])) == dict:
+                obj = objdict["{}.{}".format(args[0], args[1])]
+                for k, v in eval(args[2]).items():
+                    if (k in obj.__class__.__dict__.keys() and
+                        type(obj.__class__.__dict__[k]) in {str, int, float}):
+                        valtype = type(obj.__class__.__dict__[k])
+                        obj.__dict__[k] = valtype(v)
+            else:
+                obj.__dict__[k] = v
+            storage.save()
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
