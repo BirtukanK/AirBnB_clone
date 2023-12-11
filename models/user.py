a117135de7014a5dbac814d639dec65a66830af4
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-12-11 08:49:02 -0800

    user

--- models/user.py
+++ models/user.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 """ Defines user class"""
-import BaseModel
+from models.base_model import BaseModel
 
 class User(BaseModel):
-    """ User class that inherits from BaseModel"""
-    def __init__(self):
-        """ Initializes Usser class"""
-        super().__init__()
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+    """ User class that inherits from BaseModel""" 
+    self.email = ""
+    self.password = ""
+    self.first_name = ""
+    self.last_name = ""
