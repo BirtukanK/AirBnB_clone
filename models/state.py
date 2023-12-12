8df07d14bf3da516509885f98725bb074705bf74
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-12-12 00:35:26 -0800

    update models

--- models/state.py
+++ models/state.py
@@ -6,3 +6,6 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ A State class which defines staet of user"""
     self.name = ""
+    def __init__(self, *args, **kwargs):
+        """ Constructor """
+        super().__init__(self, *args, **kwargs)
