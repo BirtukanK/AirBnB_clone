89085b1107858394cba8f8044cce090b1bae71ce
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-12-12 05:02:17 -0800

    unittest

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,62 +1,95 @@
 #!/usr/bin/python3
-"""defines unit test for filestorage class"""
+"""test for file storage"""
 import unittest
-import os
+import pep8
 import json
+import os
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
-    """ unittest class """
-    def setUp(self):
-        """creates instance of Filestorage for testing"""
-        self.file_storage = FileStorage()
-        obj = FileStorage()
+    '''this will test the FileStorage'''
 
-    def test_file_path(self):
-        """ tests file path"""
-        obj = FileStorage()
-        file_path = obj.__file_path
-        self.assertTrue(isinstance(file_path, (dict, list)))
+    @classmethod
+    def setUpClass(cls):
+        """set up for test"""
+        cls.user = User()
+        cls.user.first_name = "Pica"
+        cls.user.last_name = "Chu"
+        cls.user.email = "1234@yahoo.com"
+        cls.storage = FileStorage()
 
-    def test_objects(self):
-        """ tests objects"""
-        obj = FileStorage()
-        obj = FileStorage()
-        result = obj.__objects
-        self.assertIsInstance(result, dict)
+    @classmethod
+    def teardown(cls):
+        """at the end of the test this will tear it down"""
+        del cls.user
+
+    def tearDown(self):
+        """teardown"""
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+
+    def test_pep8_FileStorage(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
     def test_all(self):
-        """ Tests all method"""
-        result = self.file_storage.all()
-        self.assertIsInstance(result, dict)
+        """tests if all works in File Storage"""
+        storage = FileStorage()
+        obj = storage.all()
+        self.assertIsNotNone(obj)
+        self.assertEqual(type(obj), dict)
+        self.assertIs(obj, storage._FileStorage__objects)
 
     def test_new(self):
-        """ Tests if new method adds an object to the dictionary"""
+        """test when new is created"""
+        storage = FileStorage()
+        obj = storage.all()
         user = User()
-        self.file_storage.new(user)
-        result = self.file_storage.all()
-        self.assertIn("User.{}".format(user.id), result)
+        user.id = 123455
+        user.name = "Aiko"
+        storage.new(user)
+        key = user.__class__.__name__ + "." + str(user.id)
+        self.assertIsNotNone(obj[key])
 
-    def test_save(self):
-        """ Tests save methods"""
-        user = User()
-        self.file_storage.new(user)
-        self.file_storage.save()
-        result = self.file_storage.all()
-        self.assertIn("User.{}".format(user.id), result)
-
-    def test_reload(self):
-        user = User()
-        self.file_storage.new(user)
-        self.file_storage.save()
-        new_file_storage = FileStorage()
-        new_file_storage.reload()
-
-        result = new_file_storage.all()
-        self.assertIn("User.{}".format(user.id), result)
+    def test_reload_filestorage(self):
+        """
+        tests reload
+        """
+        self.storage.save()
+        Root = os.path.dirname(os.path.abspath("console.py"))
+        path = os.path.join(Root, "file.json")
+        with open(path, 'r') as f:
+            lines = f.readlines()
+        try:
+            os.remove(path)
+        except FileNotFoundError:
+            pass
+        self.storage.save()
+        with open(path, 'r') as f:
+            lines2 = f.readlines()
+        self.assertEqual(lines, lines2)
+        try:
+            os.remove(path)
+        except FileNotFoundError:
+            pass
+        with open(path, "w") as f:
+            f.write("{}")
+        with open(path, "r") as r:
+            for line in r:
+                self.assertEqual(line, "{}")
+        self.assertIs(self.storage.reload(), None)
 
 
 if __name__ == "__main__":
